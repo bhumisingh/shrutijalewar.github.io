6a03947af437b3c347bd787d09757cb541d93c3f
Shruti Sharma <shrutijalewar@gmail.com>
2014-11-03 16:20:49 -0600

    trial 2

--- public/index.js
+++ public/index.js
@@ -4,12 +4,11 @@
   angular.module('portfolio', ['ngRoute'])
   .config(['$routeProvider', function($routeProvider){
     $routeProvider
-    .when('/', {templateUrl:'/public/views/home/home.html',             controller:'HomeCtrl'})
+    .when('/',          {templateUrl:'index.html'})
     .when('/resume',    {templateUrl:'/public/views/resume/resume.html',       controller:'ResumeCtrl'})
     .when('/contact',   {templateUrl:'/public/views/contact/contact.html',     controller:'ContactCtrl'})
     .when('/about',     {templateUrl:'/public/views/about/about.html',         controller:'AboutCtrl'})
-    .when('/projects',   {templateUrl:'/public/views/projects/projects.html',  controller:'ProjectsCtrl'})
+    .when('/projects',  {templateUrl:'/public/views/projects/projects.html',  controller:'ProjectsCtrl'})
     .otherwise({redirectTo:'/'});
   }]);
 })();
-
